19a1cbad579d7fe76bbdf5c3a73b39e9462e93f6
midxon <dixma823@student.otago.ac.nz>
2022-09-08 11:30:03 +1200

    fixed remaining errors - working version

---
+++ database-cc-setup-counters.sql
@@ -0,0 +1,11 @@
+CREATE TABLE kounters (
+    id varchar(3),
+    tally int(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO kounters VALUES ('NZD', 0);
+INSERT INTO kounters VALUES ('AUD', 0);
+INSERT INTO kounters VALUES ('USD', 0);
+INSERT INTO kounters VALUES ('GBP', 0);
+INSERT INTO kounters VALUES ('KRW', 0);
--- www/admin/index.php
+++ www/admin/index.php
@@ -1,9 +1,12 @@
 <html>
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
    // connect to DB
     $database_host = '192.168.2.12';
-    $database_name = 'currencies';
+    $database_name = 'kounters';
     $database_user = 'user';
     $databse_password = 'root123';
 
@@ -11,12 +14,13 @@
 
     $pdo = new PDO($pdo_dsn, $database_user, $databse_password);
 
-  // get the updateCount
-    $NZDcount = $pdo->query("SELECT tally from counters WHERE id = 'NZD'");
-    $AUDcount = $pdo->query("SELECT tally from counters WHERE id = 'AUD'");
-    $USDcount = $pdo->query("SELECT tally from counters WHERE id = 'USD'");
-    $GBPcount = $pdo->query("SELECT tally from counters WHERE id = 'GBP'");
-    $KRWcount = $pdo->query("SELECT tally from counters WHERE id = 'KRW'"); 
+    $NZDcount = $pdo->query("SELECT tally from kounters WHERE id = 'NZD'");
+    $AUDcount = $pdo->query("SELECT tally from kounters WHERE id = 'AUD'");
+    $USDcount = $pdo->query("SELECT tally from kounters WHERE id = 'USD'");
+    $GBPcount = $pdo->query("SELECT tally from kounters WHERE id = 'GBP'");
+    $KRWcount = $pdo->query("SELECT tally from kounters WHERE id = 'KRW'");
+  
+
 
     // set the base and currency values 
     $base = 1;
@@ -29,49 +33,49 @@
     // conditionals for each POSTed currency
     if(strcmp($_GET['currency'], "NZD" ) == 0){
       $converted = $base * $NZD;
-      $pdo->query("UPDATE counters SET tally = tally+1 WHERE id = 'NZD'");
+      $pdo->query("UPDATE kounters SET tally = tally+1 WHERE id = 'NZD'");
       echo $converted;
       }
 
-    if(strcmp($_GET['currency'], "AUD" ) == 0){
+    else if(strcmp($_GET['currency'], "AUD" ) == 0){
       $converted = $base * $AUD;
-      $pdo->query("UPDATE counters SET tally = tally+1 WHERE id = 'AUD'");
+      $pdo->query("UPDATE kounters SET tally = tally+1 WHERE id = 'AUD'");
       echo $converted;
     }
     
-    if(strcmp($_GET['currency'], "USD" ) == 0){
+    else if(strcmp($_GET['currency'], "USD" ) == 0){
       $converted = $base * $USD;
-      $pdo->query("UPDATE counters SET tally = tally+1 WHERE id = 'USD'");
+      $pdo->query("UPDATE kounters SET tally = tally+1 WHERE id = 'USD'");
       echo $converted;
       }
 
-    if(strcmp($_GET['currency'], "GBP" ) == 0){
-      $pdo->query("UPDATE counters SET tally = tally+1 WHERE id = 'GBP'");
+      else if(strcmp($_GET['currency'], "KRW" ) == 0){
+        $pdo->query("UPDATE kounters SET tally = tally+1 WHERE id = 'KRW'");
+        $converted = $base * $KRW;
+        echo $converted;
+        }
+
+   else if(strcmp($_GET['currency'], "GBP" ) == 0){
+      $pdo->query("UPDATE kounters SET tally = tally+1 WHERE id = 'GBP'");
       $converted = $base * $GBP;
       echo $converted;
       }
 
-    if(strcmp($_GET['currency'], "KRW" ) == 0){
-      $pdo->query("UPDATE counters SET tally = tally+1 WHERE id = 'KRW'");
-      $converted = $base * $KRW;
-      echo $converted;
-      }
-
     else{
-      showForm();
+      showForm($NZDcount, $AUDcount, $USDcount, $GBPcount, $KRWcount);
     }
      ?> 
     
       <?php
-      function showForm(){
+      function showForm($NZDcount, $AUDcount, $USDcount, $GBPcount, $KRWcount){
         ?>
         <h1> Admin View </h1>
         <hr>
-        <h3> Number of NZD to NZD Conversions : <?php echo $NZDcount ?> </h3>
-        <h3> Number of NZD to AUD Conversions : <?php echo $AUDcount ?> </h3>
-        <h3> Number of NZD to USD Conversions : <?php echo $USDcount ?> </h3>
-        <h3> Number of NZD to GBP Conversions : <?php echo $GBPcount ?> </h3>
-        <h3> Number of NZD to KRW Conversions : <?php echo $KRWcount ?> </h3>
+        <h3> Number of NZD to NZD Conversions : <?php echo $NZDcount->fetch()["tally"] ?> </h3>
+        <h3> Number of NZD to AUD Conversions : <?php echo $AUDcount->fetch()["tally"] ?> </h3>
+        <h3> Number of NZD to USD Conversions : <?php echo $USDcount->fetch()["tally"] ?> </h3>
+        <h3> Number of NZD to GBP Conversions : <?php echo $GBPcount->fetch()["tally"] ?> </h3>
+        <h3> Number of NZD to KRW Conversions : <?php echo $KRWcount->fetch()["tally"] ?> </h3>
       <?php
         }
       ?>
